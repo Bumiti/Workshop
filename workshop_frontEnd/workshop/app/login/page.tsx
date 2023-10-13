2eef04c66749bbf25c8e9b2ae587b3ce469608ad
nguyenhongquan <lactuong64@gmail.com>
2023-10-13 17:41:22 +0700

    demo Login Form

--- workshop_frontEnd/workshop/app/login/page.tsx
+++ workshop_frontEnd/workshop/app/login/page.tsx
@@ -3,7 +3,10 @@ import { Formik, Field, Form, FormikHelpers } from 'formik';
 import styles from './login-form.module.css';
 import * as Yup from 'yup';
 import { signIn, useSession } from "next-auth/react"
-import { useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap'
+import { AiFillGithub } from "react-icons/ai"
+import { BsFacebook } from "react-icons/bs"
+import { BiLogoGmail } from "react-icons/bi"
 type Values = {
     email: string;
     password: string;
@@ -29,9 +32,9 @@ const LoginForm = () => {
                 if (data.data.token) {
                     localStorage.setItem('token', data.data.token);
                     const tokenusung = localStorage.getItem('token');
-                    console.log("token trong local store",tokenusung)
+                    console.log("token trong local store", tokenusung)
                 }
-          
+
                 alert('Đăng nhập thành công!');
             } else {
                 alert('Đăng nhập thất bại. Vui lòng kiểm tra thông tin đăng nhập.');
@@ -42,37 +45,52 @@ const LoginForm = () => {
             setSubmitting(false);
         }
     };
-
     return (
-        <div className={styles.login_box + ' p-3'}>
-            <h1 className="display-6 mb-3">Login</h1>
-            <Formik
-                initialValues={{
-                    email: '',
-                    password: '',
-                }}
-                validationSchema={Yup.object({
-                    email: Yup.string().required('Vui lòng nhập tên đăng nhập'),
-                    password: Yup.string().required('Vui lòng nhập mật khẩu'),
-                })}
-                onSubmit={handleSubmit}
-            >
-                <Form>
-                    <div className="mb-3">
-                        <Field className="form-control" id="email" name="email" placeholder="email" aria-describedby="usernameHelp" />
+        <Container className={styles.container_css}>
+            <Row className={' p-5 my-5'}>
+                <Col col='10' md='6'>
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" className="img-fluid" alt="Phone image" />
+                </Col>
+                <Col col='4' md='6' >
+                    <div className={' p-3'}>
+                        <h1 className="display-6 mb-3">Login</h1>
+                        <Formik
+                            initialValues={{
+                                email: '',
+                                password: '',
+                            }}
+                            validationSchema={Yup.object({
+                                email: Yup.string().required('Vui lòng nhập tên đăng nhập'),
+                                password: Yup.string().required('Vui lòng nhập mật khẩu'),
+                            })}
+                            onSubmit={handleSubmit}
+                        >
+                            <Form>
+                                <div className="mb-3">
+                                    <Field className="form-control" id="email" name="email" placeholder="email" aria-describedby="usernameHelp" />
+                                </div>
+                                <div className="mb-3">
+                                    <Field className="form-control" id="password" name="password" placeholder="Password" type="password" />
+                                </div>
+                                <button className="mb-4 w-100 btn btn-primary" type="submit">Login</button>
+                                <h2 className="lead fw-normal mb-0 me-3 text-center">Sign in with</h2>
+                                <div className={styles.div_media}>
+                                    <button onClick={() => signIn("facebook")} className='btn btn-lg me-2'>
+                                        <BsFacebook fab="true" icon='facebook-f' />
+                                    </button>
+                                    <button onClick={() => signIn("github")} className='btn  btn-lg me-2'>
+                                        <AiFillGithub fab="true" icon='github' />
+                                    </button>
+                                    <button onClick={() => signIn("google")} className='btn  btn-lg me-2'>
+                                        <BiLogoGmail fab="true" icon='mail' />
+                                    </button>
+                                </div>
+                            </Form>
+                        </Formik>
                     </div>
-
-                    <div className="mb-3">
-                        <Field className="form-control" id="password" name="password" placeholder="Password" type="password" />
-                    </div>
-
-                    <button type="submit" className="btn btn-primary">Login</button>
-                    <button onClick={() => signIn("google")} >Login With Google</button>
-                    <button onClick={() => signIn("facebook")} className="btn btn-primary">Login With FaceBook</button>
-                    <button onClick={() => signIn("github")} className="btn btn-primary">Login With GitHub</button>
-                </Form>
-            </Formik>
-        </div>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 export default LoginForm;
